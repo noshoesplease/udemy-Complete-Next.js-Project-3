fe6b9fc8e209911ba9442827d574bd77e34afb79
Devin Custodio <devin.roy.custodio@gmail.com>
2024-02-17 14:17:44 -0500

    updates

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,6 +1,28 @@
+import React, { useState } from "react";
 import Link from "next/link";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+} from "reactstrap";
+
+const BsNavLink = (props) => {
+  const { href, title } = props;
+  return (
+    <>
+      <Link className="nav-link port-navbar-link" href={href}>
+        {title}
+      </Link>
+    </>
+  );
+};
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
   const links = [
     { title: "Home", path: "/" },
     { title: "Blogs", path: "/blogs" },
@@ -8,15 +30,31 @@ const Header = () => {
     { title: "About", path: "/about" },
     { title: "CV", path: "/cv" },
   ];
-
   return (
-    <>
-      {links.map((link, index) => (
-        <Link key={index} href={link.path}>
-          {link.title}
-        </Link>
-      ))}
-    </>
+    <div>
+      <Navbar
+        className="port-navbar port-default absolute"
+        color="transparent"
+        dark
+        expand="md"
+      >
+        <NavbarBrand>
+          {/* <Link className="port-navbar-brand" href="/">
+            Devin Custodio
+          </Link> */}
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            {links.map((link, index) => (
+              <NavItem key={index} className="port-navbar-item">
+                <BsNavLink href={link.path} title={link.title} />
+              </NavItem>
+            ))}
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
   );
 };
 
