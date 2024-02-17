b9bf5dac027ea3925295e655f2058635907081f7
Devin Custodio <devin.roy.custodio@gmail.com>
2024-02-17 13:18:17 -0500

    added portfolios content

--- src/pages/portfolios.jsx
+++ src/pages/portfolios.jsx
@@ -1,8 +1,27 @@
 import BaseLayout from "@/components/layouts/BaseLayout";
-export default function PortfoliosPage() {
+import axios from "axios";
+
+export default function PortfoliosPage({ portfolios }) {
   return (
     <BaseLayout>
       <h1>Portfolios Page</h1>
+      <ul>
+        {portfolios.map((portfolio) => (
+          <li key={portfolio.id}>{portfolio.title}</li>
+        ))}
+      </ul>
     </BaseLayout>
   );
 }
+
+PortfoliosPage.getInitialProps = async () => {
+  let portfolios = [];
+  try {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    portfolios = res.data;
+  } catch (e) {
+    console.error(e);
+  }
+
+  return { portfolios: portfolios.slice(0, 10) };
+};
